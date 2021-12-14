dda3af0542f4e57c7d4a65e60c81fdb05ee0cdcc
lwarode <lukas.warode@gmx.de>
2021-12-14 20:36:35 +0100

    nothing

--- global.R
+++ global.R
@@ -6,7 +6,7 @@ library(legislatoR)
 library(rgdal)
 library(sp)
 
-#  Load Scripts ----------------------------------------------------------
+# Load Scripts ------------------------------------------------------------
 
 # Load SpatialPolygon data for map
 source("shapefile_loader.R")
