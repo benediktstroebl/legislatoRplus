e902ea673edbbf602473e48088187ee7a7af2407
lwarode <lukas.warode@gmx.de>
2021-12-15 19:09:42 +0100

    added loading spinner

---
+++ ui.R
@@ -0,0 +1,37 @@
+library(shiny)
+
+ui <- fluidPage(
+  
+  # Session input
+  selectInput("session_input", "Select Legislative Sessions", session_list, multiple = TRUE),
+  
+  # Party input
+  selectInput("party_input", "Select Parties", party_list, multiple = TRUE),
+  
+  # Name input
+  selectInput("name_input", "MP Name", mp_list, selected = FALSE, multiple = FALSE),
+  
+  # verbatimTextOutput("name_list_reactive_output"),
+  
+  # Leaflet map
+  leafletOutput(outputId = "mymap"),
+  
+  br(),
+  
+  fluidRow(
+    column(
+      8,
+      plotOutput("age_plot_final") %>% 
+        withSpinner(color = "black"),
+      plotOutput("session_plot_final") %>% 
+        withSpinner(color = "black")
+    ),
+    column(
+      4,
+      verbatimTextOutput("debug_radar_plot_df"),
+      plotOutput("radar_plot_final") %>% 
+        withSpinner(color = "black")
+    )
+  )
+  
+)
